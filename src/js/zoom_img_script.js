0b02931f74defcfee6c6b1d75c3d0d0e857c7481
Physicus <physicus@bk.ru>
2023-10-02 18:40:33 +0300

    Added proportional zoom

--- src/js/zoom_img_script.js
+++ src/js/zoom_img_script.js
@@ -2,7 +2,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const image_modal = document.querySelector('.img_modal__Image'),
     close = document.querySelector('.overlay .img_modal__close'),
 	dateImg = document.querySelectorAll('.paint_img'),
-    overlay = document.querySelector('.overlay');
+    overlay = document.querySelector('.overlay'),
+    scroll = calcScroll();
     // console.log(image_modal);
 
     dateImg.forEach(item => {
@@ -14,6 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
             overlay.style.display = "block";
             document.body.style.overflow = "hidden";
+            document.body.style.marginRight = `${scroll}px`;
             // console.log(e.target.src);
             // console.log(image_modal.src);
             image_modal.src = e.target.src;                
@@ -24,14 +26,29 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
+    function calcScroll () {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;/*  Ширина полосы прокрутки */
+        div.remove();
+
+        return scrollWidth;
+    }
+
 
     close.addEventListener('click', () =>{
         overlay.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
     });
 
     overlay.addEventListener('click', () =>{
         overlay.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
     });      
 });
